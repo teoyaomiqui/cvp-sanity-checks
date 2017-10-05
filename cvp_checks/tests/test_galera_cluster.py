3d0b220d0a3b68a15fc5085cbcca61f3b0296073
Oleksii Zhurba <ozhurba@mirantis.com>
2017-10-05 16:52:01 -0500

    Fixing galera test minion targeting

--- cvp_checks/tests/test_galera_cluster.py
+++ cvp_checks/tests/test_galera_cluster.py
@@ -1,6 +1,6 @@
 def test_galera_cluster_status(local_salt_client):
     gs = local_salt_client.cmd(
-        'I@galera:*',
+        'galera:*',
         'cmd.run',
         ['salt-call mysql.status | grep -A1 wsrep_cluster_size | tail -n1'],
         expr_form='pillar')
