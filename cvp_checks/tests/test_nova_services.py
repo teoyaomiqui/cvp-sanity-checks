5dc7dc7160097adf8dd6c4901d073a3926f1a775
Oleksii Zhurba <legan4ik@gmail.com>
2017-10-31 19:10:32 +0000

    Add test to check nova services status

---
+++ cvp_checks/tests/test_nova_services.py
@@ -0,0 +1,8 @@
+def test_nova_services_status(local_salt_client):
+    result = local_salt_client.cmd(
+        'keystone:server',
+        'cmd.run',
+        ['. /root/keystonerc; nova service-list | grep "down\|disabled"'],
+        expr_form='pillar')
+    assert result[result.keys()[0]] == '', \
+        '''Some nova services are in wrong state'''
