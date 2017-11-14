ad02010ccc7e8f72e42ffb3bcac33e3e229d27df
Oleksii Zhurba <ozhurba@mirantis.com>
2017-11-14 16:30:38 -0600

    Exclude 'disable on boot' contrail-status line

--- cvp_checks/tests/test_contrail.py
+++ cvp_checks/tests/test_contrail.py
@@ -35,12 +35,12 @@ def test_contrail_compute_status(local_salt_client):
 def test_contrail_node_status(local_salt_client):
     cs = local_salt_client.cmd(
         'opencontrail:client:analytics_node', 'cmd.run',
-        ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support)\''],
+        ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support|boot)\''],
         expr_form='pillar'
     )
     cs.update(local_salt_client.cmd(
         'opencontrail:control', 'cmd.run',
-        ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support)\''],
+        ['contrail-status | grep -Pv \'(==|^$|Disk|unix|support|boot)\''],
         expr_form='pillar')
     )
     if not cs:
