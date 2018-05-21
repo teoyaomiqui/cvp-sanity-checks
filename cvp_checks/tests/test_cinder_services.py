656157dc9bd237f0db40967ae8e80d12f5064942
Oleksii Zhurba <ozhurba@mirantis.com>
2018-05-21 13:38:51 -0500

    Skip cinder test if cinder is not found

--- cvp_checks/tests/test_cinder_services.py
+++ cvp_checks/tests/test_cinder_services.py
@@ -1,9 +1,14 @@
+import pytest
+
+
 def test_cinder_services(local_salt_client):
     service_down = local_salt_client.cmd(
-        'keystone:server',
+        'cinder:controller',
         'cmd.run',
         ['. /root/keystonerc; cinder service-list | grep "down\|disabled"'],
         expr_form='pillar')
+    if not service_down:
+        pytest.skip("Cinder is not found on this environment")
     cinder_volume = local_salt_client.cmd(
         'keystone:server',
         'cmd.run',
